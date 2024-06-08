701f25130950e0c48f3b0d0fc609d69f206af5b2
nandanugg <nandanugg@gmail.com>
2024-06-08 09:36:40 +0700

    add logging

--- k6/tests/purchase/estimateOrderTest.js
+++ k6/tests/purchase/estimateOrderTest.js
@@ -155,6 +155,7 @@ export function EstimateOrderTest(user, admin, zone1, zone2, config, tags) {
             ['should return 400']: (v) => v.status === 400
         }, config, tags)
 
+        console.log('positivePayload:', positivePayload)
         const testObjects = generateTestObjects({
             userLocation: {
                 type: 'object',
